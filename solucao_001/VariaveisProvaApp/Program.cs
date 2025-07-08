54ec4dca7604c96eb5b5c48a6e3a748d537fdd17
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2025-07-08 15:27:31 -0300

    code challenge

--- solucao_001/VariaveisProvaApp/Program.cs
+++ solucao_001/VariaveisProvaApp/Program.cs
@@ -4,16 +4,9 @@ class Program
 {
     static void Main()
     {
-        // Declaração das variáveis
-        string nomeProduto = "Notebook";
-        int quantidade = 15;
-        double precoUnitario = 2850.90;
-        bool ativoParaVenda = true;
-
-        // Exibição das informações no console usando concatenação
-        Console.WriteLine("Produto: " + nomeProduto);
-        Console.WriteLine("Quantidade em estoque: " + quantidade);
-        Console.WriteLine("Preço unitário: R$" + precoUnitario);
-        Console.WriteLine("Ativo para venda: " + ativoParaVenda);
+        string nomeUsuario;
+        int idadeCliente;
+        double valorTotalPedido;
+        bool cadastroAtivo;
     }
 }
