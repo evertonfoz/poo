afed6e57a86ee36178cf7704423d122dbbe1254a
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2025-11-05 09:32:27 -0300

    pdfs

--- solucao_001/VariaveisProvaApp/obj/Debug/net9.0/VariaveisProvaApp.AssemblyInfo.cs
+++ solucao_001/VariaveisProvaApp/obj/Debug/net9.0/VariaveisProvaApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VariaveisProvaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f257042eab7013b9d163a47d085581c4bc61db6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1791804cb3adca33810fe38ae088c2262e4d5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("VariaveisProvaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VariaveisProvaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
